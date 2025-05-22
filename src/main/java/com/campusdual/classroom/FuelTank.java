8361debd724a74b557370783be61d0de46f06d2e
NotOppi <Filippofg@outlook.com>
2025-05-22 14:38:32 +0200

    Listo

--- src/main/java/com/campusdual/classroom/FuelTank.java
+++ src/main/java/com/campusdual/classroom/FuelTank.java
@@ -2,13 +2,21 @@ package com.campusdual.classroom;
 
 public class FuelTank {
 
-  public int actualFuel = 10;
+  private int actualFuel = 10;
 
   public void showDetails() {
-    System.out.println("La capacidad actual es de " + this.actualFuel + " litros.");
+    System.out.println("La capacidad actual es de " + getActualFuel() + " litros.");
   }
 
   public int getActualFuel() {
     return this.actualFuel;
   }
+
+  public void setActualFuel(int fuel) {
+    if (fuel >= 0)
+      this.actualFuel = fuel;
+    else
+      System.out.println("Cannot set fuel value to be less than zero.");
+  }
+
 }
